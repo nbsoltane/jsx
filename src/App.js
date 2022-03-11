c9b7d68bae2d9451584bf90208f5b9a8fa8d5e86
nbsoltane <nbsoltane@gmail.com>
2022-03-11 22:57:47 +0100

    form boostrap

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import FormHome from './components/FormHome'
+
+function App() {
+  return (
+    <div className="formBody">    
+      <div className="container">
+        <div className="form">
+          <FormHome />
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/FormHome.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Form, Button } from 'react-bootstrap'
+
+export default function FormHome() {
+  return (
+    <div>
+         <Form>
+         <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="name" placeholder="Name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control type="password" placeholder="confirm Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  )
+}
